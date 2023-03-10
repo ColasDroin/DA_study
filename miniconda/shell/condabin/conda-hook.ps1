08695fffa2adbdd387aa2f6940591f81d2e1c400
Colas <colasdroin@gmail.com>
2023-03-10 13:29:55 +0100

    Add miniconda

---
+++ miniconda/shell/condabin/conda-hook.ps1
@@ -0,0 +1,9 @@
+$Env:CONDA_EXE = "/home/cdroin/DA_study/miniconda/bin/conda"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_ROOT = "/home/cdroin/DA_study/miniconda"
+$Env:_CONDA_EXE = "/home/cdroin/DA_study/miniconda/bin/conda"
+$CondaModuleArgs = @{ChangePs1 = $True}
+Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
+
+Remove-Variable CondaModuleArgs
