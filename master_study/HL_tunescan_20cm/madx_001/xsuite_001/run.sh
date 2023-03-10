473c3089e9573913feb8d25dc8de34a2d9d6c5ca
Colas <colasdroin@gmail.com>
2023-03-10 12:04:16 +0100

    Changed structure

--- master_study/HL_tunescan_20cm/madx_001/xsuite_001/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/cdroin/example_DA_study/master_study/../../miniconda3/bin/activate
-cd /home/cdroin/example_DA_study/master_study/HL_tunescan_20cm/madx_001/xsuite_001
-python 000_track.py > output.txt 2> error.txt
-rm -rf final_* modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log __pycache__ twiss* errors fc* optics_orbit_at*
