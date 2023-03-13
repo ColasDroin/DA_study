7108744dcfe5d75bef279cd36df7e3356ffb227f
Colas <colasdroin@gmail.com>
2023-03-13 12:18:29 +0100

    Small update

--- master_study/clean_it.sh
+++ master_study/clean_it.sh
@@ -1,6 +1,9 @@
-rm -rf 000_*
-rm -rf __pycache__
-rm -f tree_maker*
-rm -f run.sh out output.txt error.txt
-rm -f *.sub
-rm *parquet
+#rm 000_pymask.py
+#rm optics_specific_tools.py
+rm -f twiss_*
+rm -f final_*
+rm -f fc.*
+rm -f *.log
+rm -f *json
+rm -rf temp __pycache__ xsuite_lines
+rm -f tools tracking_tools optics_toolkit errors beambeam_macros modules optics_repository
--- master_study/user_defined_functions.py
+++ master_study/user_defined_functions.py
@@ -15,3 +15,10 @@ def generate_run_sh_htc(node, generation_number):
         " modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log"
         " __pycache__ twiss* errors fc* optics_orbit_at*\n"
     )
+    # return (f'#!/bin/bash\n'
+    #        f'source {node.root.parameters["setup_env_script"]}\n'
+    #        f'cp -rf {node.get_abs_path()}/* .\n'
+    #        f'ls\n'
+    #        f'pwd\n'
+    #        f'python {python_command} > output_ht.txt 2> error_ht.txt\n'
+    #        f'rm -rf final_* modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log __pycache__ twiss* errors fc* optics_orbit_at*\n')
