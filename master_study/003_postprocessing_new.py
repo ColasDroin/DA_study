c56f046662bb240c87a02c9aa5b0ac9077afa73b
Colas <colasdroin@gmail.com>
2023-03-15 16:59:13 +0100

    Updated analysis

--- master_study/003_postprocessing_new.py
+++ master_study/003_postprocessing_new.py
@@ -33,73 +33,60 @@ except Exception as e:
     print("Probably you forgot to edit the address of you json file...")
 
 my_list = []
-# if root.has_been('completed'):
-#    print('All descendants of root are completed!')
-if True:
-    for node in root.generation(2):
-        # print(node, root.get_abs_path())
-        # quit()
-        # node_df = pd.read_parquet(f'{node.get_abs_path()}/final_summ_BBOFF.parquet')
-        with open(f"{node.get_abs_path()}/config.yaml", "r") as fid:
-            config_parent = yaml.safe_load(fid)
-        # node_df['path']= f'{node.get_abs_path()}'
-        for node_child in node.children:
-            # os.sytem(f'bsub cd {node.path} &&  {node.path_template} ')
-            # my_list.append(pd.read_parquet(f'{node.path}/test.parquet', columns=['x']).iloc[-1].x)
-            with open(f"{node_child.get_abs_path()}/config.yaml", "r") as fid:
-                config = yaml.safe_load(fid)
+
+for node in root.generation(2):
+    with open(f"{node.get_abs_path()}/config.yaml", "r") as fid:
+        config_parent = yaml.safe_load(fid)
+    for node_child in node.children:
+        with open(f"{node_child.get_abs_path()}/config.yaml", "r") as fid:
+            config = yaml.safe_load(fid)
+        try:
+            particle = pd.read_parquet(f"{node_child.get_abs_path()}/{config['particle_file']}")
+            df = pd.read_parquet(f"{node_child.get_abs_path()}/output_particles.parquet")
+            df["path 1"] = f"{node.get_abs_path()}"
+            df["name 1"] = f"{node.name}"
+            df["path 2"] = f"{node_child.get_abs_path()}"
+            df["name 2"] = f"{node_child.name}"
+            df["q1"] = config_parent["qx0"]
+            df["q2"] = config_parent["qy0"]
+            df["nb"] = node.parameters["beam_npart"]
+            df["oct_current"] = node.parameters["oct_current"]
+            df["on_x1"] = node.parameters["knob_settings"]["on_x1"]
             try:
-                # print(f"{node_child.get_abs_path()}/{config['particle_file']}")
-                # print(f"{node_child.get_abs_path()}/output_particles.parquet")
+                df["bunch_nb"] = node.parameters["beambeam_config"]["bunch_to_track"]
+            except:
+                df["bunch_nb"] = config_parent["beambeam_config"]["bunch_to_track"]
+            df = pd.merge(df, particle, on=["particle_id"])
+            my_list.append(df)
+        except Exception as e:
+            problematic.append(node_child.get_abs_path())
+            # print(f"problem {node_child.get_abs_path()} ", e)
 
-                particle = pd.read_parquet(f"{node_child.get_abs_path()}/{config['particle_file']}")
-                df = pd.read_parquet(f"{node_child.get_abs_path()}/output_particles.parquet")
-                df["path 1"] = f"{node.get_abs_path()}"
-                df["name 1"] = f"{node.name}"
-                df["path 2"] = f"{node_child.get_abs_path()}"
-                df["name 2"] = f"{node_child.name}"
-                # df['q1 final']=node_df['q1'].values[0]
-                # df['q2 final']=node_df['q2'].values[0]
-                df["q1"] = config_parent["qx0"]
-                df["q2"] = config_parent["qy0"]
-                df["nb"] = node.parameters["beam_npart"]
-                df["oct_current"] = node.parameters["oct_current"]
-                df["on_x1"] = node.parameters["knob_settings"]["on_x1"]
-                try:
-                    df["bunch_nb"] = node.parameters["beambeam_config"]["bunch_to_track"]
-                except:
-                    df["bunch_nb"] = config_parent["beambeam_config"]["bunch_to_track"]
-                df = pd.merge(df, particle, on=["particle_id"])
-                my_list.append(df)
-            except Exception as e:
-                problematic.append(node_child.get_abs_path())
-                # print(f"problem {node_child.get_abs_path()} ", e)
-
-    my_df = pd.concat(my_list)
-    aux = my_df[my_df["state"] != 1]  # unstable
-    df_for_evolution = aux.copy()
-    print(
-        pd.DataFrame(
-            [
-                aux.groupby("name 1")["normalized amplitude in xy-plane"].min(),
-                aux.groupby("name 1")["q1"].mean(),
-                aux.groupby("name 1")["q2"].mean(),
-            ]
-        ).transpose()
-    )
-    my_final = pd.DataFrame(
+my_df = pd.concat(my_list)
+aux = my_df[my_df["state"] != 1]  # unstable
+df_for_evolution = aux.copy()
+print(
+    pd.DataFrame(
         [
             aux.groupby("name 1")["normalized amplitude in xy-plane"].min(),
             aux.groupby("name 1")["q1"].mean(),
             aux.groupby("name 1")["q2"].mean(),
-            aux.groupby("name 1")["nb"].mean(),
-            aux.groupby("name 1")["on_x1"].mean(),
-            aux.groupby("name 1")["oct_current"].mean(),
-            aux.groupby("name 1")["bunch_nb"].mean(),
         ]
     ).transpose()
-    my_final.to_parquet(f"{my_study}/da.parquet")
-    df_for_evolution.to_parquet(f"{my_study}/da_evolution.parquet")
+)
+my_final = pd.DataFrame(
+    [
+        aux.groupby("name 1")["normalized amplitude in xy-plane"].min(),
+        aux.groupby("name 1")["q1"].mean(),
+        aux.groupby("name 1")["q2"].mean(),
+        aux.groupby("name 1")["nb"].mean(),
+        aux.groupby("name 1")["on_x1"].mean(),
+        aux.groupby("name 1")["oct_current"].mean(),
+        aux.groupby("name 1")["bunch_nb"].mean(),
+    ]
+).transpose()
+my_final.to_parquet(f"{my_study}/da.parquet")
+df_for_evolution.to_parquet(f"{my_study}/da_evolution.parquet")
 
 end = time.time()
 print(end - start)
